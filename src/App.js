5a81b6f6b6fde9379954f579033b7adf34b3bfe5
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-01-19 16:42:03 -0300

    Adiciona requisito 03

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './pages/Search';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/Cart" component={ Cart } />
         <Route exact path="/" component={ Search } />
       </Switch>
     </BrowserRouter>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Search extends Component {
   render() {
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Link to="/Cart" data-testid="shopping-cart-button"> Carrinho </Link>
+      </div>
     );
   }
 }
