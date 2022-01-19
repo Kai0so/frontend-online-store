c2cfad3e0696b7e36349bff723371922c34bb1cf
TiLourinho <lourinho.tiago@gmail.com>
2022-01-19 17:45:18 -0300

    Requisito 4: agora vai!

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const categoriesList = await getCategories();
+    console.log(categoriesList);
+    this.setState({ categories: categoriesList });
+  }
+
   render() {
+    const { categories } = this.state;
+
     return (
-      <div>
+      <main>
+        <ul>
+          { categories.map((elem) => (
+            <li key={ elem.id }>
+              <button type="button" data-testid="category">
+                { elem.name }
+              </button>
+            </li>
+          ))}
+        </ul>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <Link to="/Cart" data-testid="shopping-cart-button"> Carrinho </Link>
-      </div>
+      </main>
     );
   }
 }
